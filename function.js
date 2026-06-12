6e3647fca54467cf4e4af5eacf4cd16be511e794
deepikasri-31 <deepikasrib.n@gmail.com>
2026-06-12 15:46:44 +0530

    added function.js

---
+++ function.js
@@ -0,0 +1,13 @@
+// Non parametric function
+function greek(){
+    console.log("Hello, world!");
+}
+greek(); // output:Hello,world!
+
+// paramatric function
+function add(a,b){
+    return a + b;
+}
+console.log(add(5,3));
+
+    
