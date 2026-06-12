beb7a0e9f6c410cddbc1a4ffc0e413b1d80a2930
deepikasri-31 <deepikasrib.n@gmail.com>
2026-06-12 14:04:22 +0530

    added operators.js

--- operators.js
+++ operators.js
@@ -27,16 +27,29 @@ console.log(a>=b);
 console.log(a<=b);
 
 //logical operators
+console.log("logical operators");
 console.log(a>0 && b>0); 
 console.log(a>0 && b<0); 
 console.log(a<0||b<0);
 console.log(!(a>0));
 
 //unary operators
+console.log("unary operators");
 console.log(c++);
 console.log(c--);
 console.log(--c);
 console.log(++c);
+
+//ternary operators 
+console.log("ternary operators");
+var result = (a > b) ? "a is graeter" : "b is greater"
+console.log(result); // o/p -b is grater
+ 
+
+
+
+
+
 
 
 
