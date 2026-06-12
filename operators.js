81ba85b1b8732ad85d4d095b521bd76cd9077767
deepikasri-31 <deepikasrib.n@gmail.com>
2026-06-12 11:24:47 +0530

    added operators.js

---
+++ operators.js
@@ -0,0 +1,30 @@
+//Arthematic
+var a = 10;
+var b = 20;
+console.log(a+b);//add
+console.log(a-b);//sub
+console.log(a*b);//multiplication
+console.log(a/b);//div
+console.log(a%b);//mod
+console.log(a**b);//exp
+
+console.log("Assignment operator");
+var c = 30;
+console.log(c+=10);
+console.log(c-=10);
+console.log(c*=10);
+console.log(c/=10);
+console.log(c%=10);
+console.log(c**=10);
+
+
+
+
+
+
+
+
+
+
+
+
