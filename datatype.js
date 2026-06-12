1fd16b0579585dcb0626d163c047af0c6849d131
deepikasri-31 <deepikasrib.n@gmail.com>
2026-06-12 14:46:42 +0530

    added datatype.js

---
+++ datatype.js
@@ -0,0 +1,29 @@
+console.log("Wlecome to primitive datatype");
+//numbers
+console.log("Dt numbers");
+var age = 30;
+console.log(age);
+
+//string
+console.log("Dt string");
+var name = "deepika";
+console.log(name);
+
+//boolean 
+console.log("Dt boolean");
+var isStudent = true;
+console.log(isStudent);
+
+//null
+console.log("Dt null");
+var emotyvalue = null;
+console.log(emptyvalue);
+
+//undefine
+console.log("Dt undefine");
+var undefine = undefine;
+console.log(undefined);
+
+
+
+
