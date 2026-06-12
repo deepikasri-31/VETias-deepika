b471fe048f76309d7bd273435ca018297fb232ae
deepikasri-31 <deepikasrib.n@gmail.com>
2026-06-12 16:03:14 +0530

    added conditional.js

---
+++ conditional.js
@@ -0,0 +1,16 @@
+var light = "white"
+
+if(light == "green"){
+    console.log("go!");
+}
+else if(light == "yellow"){
+    console.log("wait!");
+}
+else if(light == "red"){
+    console.log("stop!");
+    
+}
+else {
+    console.log("invalied light color");    
+}
+console.log("outside if");
