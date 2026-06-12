7803dfa03bbed40e4797ecbc5c70d95f0f1ba085
deepikasri-31 <deepikasrib.n@gmail.com>
2026-06-12 11:31:58 +0530

    added variable.js

---
+++ variable.js
@@ -0,0 +1,14 @@
+//Arithematic
+var a = 10;
+var b = 20;
+console.log(a+b);//add
+console.log(a-b);//sub
+console.log(a*b);//multiplication
+console.log(a/b);//div
+console.log(a%b);//mod
+console.log(a**b);//expo
+
+
+
+
+
